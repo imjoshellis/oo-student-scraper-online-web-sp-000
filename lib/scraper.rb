2abd8f0007e8296e21e5f28edb4421d87e8e971c
Josh Ellis <joshe523@gmail.com>
2020-03-07 00:15:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     @students = []
     @doc = Nokogiri::HTML(open(index_url))
-    arr = @doc.css(".student-name").collect
+    arr = @doc.css(".student-name")
     binding.pry
     return @students
   end
