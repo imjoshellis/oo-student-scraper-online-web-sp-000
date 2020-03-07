482796c183604ef78be0894f892502eb9a95ca94
Josh Ellis <joshe523@gmail.com>
2020-03-07 00:13:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,11 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    @students = []
+    @doc = Nokogiri::HTML(open(index_url))
+    arr = @doc.css(".student-name").collect
+    binding.pry
+    return @students
   end
 
   def self.scrape_profile_page(profile_url)
